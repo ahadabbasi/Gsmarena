2e3946565da7b2ae4658de0916bfc145a9940df4
Ahad Abbasi <ahadabbasi@gmail.com>
2024-02-11 15:32:52 +0330

    Create context for application

---
+++ Gsmarena.WindowsApplication/Models/Repositories/ApplicationContext.cs
@@ -0,0 +1,71 @@
+﻿namespace Gsmarena.WindowsApplication.Models.Repositories;
+
+public class ApplicationContext
+{
+    public string ConnectionString { get; }
+
+    public ApplicationContext(string connectionString)
+    {
+        ConnectionString = connectionString;
+    }
+
+    private BrandRepository? _brand;
+
+    public BrandRepository Brand
+    {
+        get
+        {
+            if (_brand == null)
+            {
+                _brand = new BrandRepository(ConnectionString);
+            }
+
+            return _brand;
+        }
+    }
+    
+    private DeviceRepository? _device;
+
+    public DeviceRepository Device
+    {
+        get
+        {
+            if (_device == null)
+            {
+                _device = new DeviceRepository(ConnectionString);
+            }
+
+            return _device;
+        }
+    }
+    
+    private NetworkRepository? _network;
+
+    public NetworkRepository Network
+    {
+        get
+        {
+            if (_network == null)
+            {
+                _network = new NetworkRepository(ConnectionString);
+            }
+
+            return _network;
+        }
+    }
+    
+    private OperationSystemRepository? _operationSystem;
+
+    public OperationSystemRepository OperationSystem
+    {
+        get
+        {
+            if (_operationSystem == null)
+            {
+                _operationSystem = new OperationSystemRepository(ConnectionString);
+            }
+
+            return _operationSystem;
+        }
+    }
+}
