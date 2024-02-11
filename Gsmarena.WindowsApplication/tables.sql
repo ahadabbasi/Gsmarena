36b14810a9802c2292e9ab29477e30df23db07c1
Ahad Abbasi <ahadabbasi@gmail.com>
2024-02-11 14:40:23 +0330

    Change to new schema

--- Gsmarena.WindowsApplication/tables.sql
+++ Gsmarena.WindowsApplication/tables.sql
@@ -1,10 +1,12 @@
-﻿CREATE TABLE `brands`
+﻿
+CREATE TABLE `brands`
 (
     `id`   INTEGER AUTO_INCREMENT NOT NULL,
     `name` NVARCHAR(200)          NOT NULL,
     PRIMARY KEY (`id`)
 )
-    CHARACTER SET `utf8mb4` COLLATE `utf8mb4_general_ci`;
+    CHARACTER SET `utf8mb4`
+    COLLATE `utf8mb4_general_ci`;
 
 CREATE TABLE `networks`
 (
@@ -12,7 +14,8 @@ CREATE TABLE `networks`
     `name` NVARCHAR(200)          NOT NULL,
     PRIMARY KEY (`id`)
 )
-    CHARACTER SET `utf8mb4` COLLATE `utf8mb4_general_ci`;
+    CHARACTER SET `utf8mb4`
+    COLLATE `utf8mb4_general_ci`;
 
 CREATE TABLE `technologies`
 (
@@ -20,27 +23,43 @@ CREATE TABLE `technologies`
     `name` NVARCHAR(200)          NOT NULL,
     PRIMARY KEY (`id`)
 )
-    CHARACTER SET `utf8mb4` COLLATE `utf8mb4_general_ci`;
+    CHARACTER SET `utf8mb4`
+    COLLATE `utf8mb4_general_ci`;
+
+
+CREATE TABLE `operation_systems`
+(
+    `id`   INTEGER AUTO_INCREMENT NOT NULL,
+    `name` NVARCHAR(200)          NOT NULL,
+    PRIMARY KEY (`id`)
+)
+    CHARACTER SET `utf8mb4`
+    COLLATE `utf8mb4_general_ci`;
 
 CREATE TABLE `devices`
 (
-    `id`               INTEGER AUTO_INCREMENT          NOT NULL,
-    `name`             NVARCHAR(200)                   NOT NULL,
-    `brand_id`         INTEGER                         NOT NULL,
-    `type`             ENUM ('PHONE','WATCH','TABLET') NOT NULL,
-    `display_ratio`    DECIMAL                         NOT NULL,
-    `display_size`     DECIMAL                         NOT NULL,
-    `weight`           DECIMAL                         NOT NULL,
-    `battery_capacity` INTEGER UNSIGNED                NOT NULL,
-    `pixel_per_inches` INTEGER UNSIGNED                NOT NULL,
-    `processor_model`  NVARCHAR(200)                   NOT NULL,
-    `count_of_thread`  NVARCHAR(200)                   NOT NULL,
-    `price`            DECIMAL                         NOT NULL,
-    `year_of_release`  DATE                            NOT NULL,
+    `id`                       INTEGER AUTO_INCREMENT          NOT NULL,
+    `url`                      NVARCHAR(200)                   NOT NULL,
+    `name`                     NVARCHAR(200)                   NOT NULL,
+    `brand_id`                 INTEGER                         NOT NULL,
+    `type`                     ENUM ('PHONE','WATCH','TABLET') NOT NULL,
+    `display_ratio`            DECIMAL                         NOT NULL,
+    `display_size`             DECIMAL                         NOT NULL,
+    `weight`                   DECIMAL                         NOT NULL,
+    `battery_capacity`         INTEGER UNSIGNED                NOT NULL,
+    `pixel_per_inches`         INTEGER UNSIGNED                NOT NULL,
+    `processor_model`          NVARCHAR(200)                   NOT NULL,
+    `count_of_thread`          NVARCHAR(200)                   NOT NULL,
+    `price`                    DECIMAL                         NOT NULL,
+    `year_of_release`          DATE                            NOT NULL,
+    `operation_id`             INTEGER                         NOT NULL,
+    `operation_system_version` NVARCHAR(200)                   NULL,
     PRIMARY KEY (`id`),
-    FOREIGN KEY (`brand_id`) REFERENCES `brands` (`id`)
+    FOREIGN KEY (`brand_id`) REFERENCES `brands` (`id`),
+    FOREIGN KEY (`operation_id`) REFERENCES `operation_systems` (`id`)
 )
-    CHARACTER SET `utf8mb4` COLLATE `utf8mb4_general_ci`;
+    CHARACTER SET `utf8mb4`
+    COLLATE `utf8mb4_general_ci`;
 
 CREATE TABLE `devices_dimensions`
 (
@@ -52,7 +71,8 @@ CREATE TABLE `devices_dimensions`
     PRIMARY KEY (`id`),
     FOREIGN KEY (`device_id`) REFERENCES `devices` (`id`)
 )
-    CHARACTER SET `utf8mb4` COLLATE `utf8mb4_general_ci`;
+    CHARACTER SET `utf8mb4`
+    COLLATE `utf8mb4_general_ci`;
 
 CREATE TABLE `devices_cameras`
 (
@@ -64,7 +84,8 @@ CREATE TABLE `devices_cameras`
     PRIMARY KEY (`id`),
     FOREIGN KEY (`device_id`) REFERENCES `devices` (`id`)
 )
-    CHARACTER SET `utf8mb4` COLLATE `utf8mb4_general_ci`;
+    CHARACTER SET `utf8mb4`
+    COLLATE `utf8mb4_general_ci`;
 
 CREATE TABLE `devices_networks`
 (
@@ -75,24 +96,5 @@ CREATE TABLE `devices_networks`
     FOREIGN KEY (`device_id`) REFERENCES `devices` (`id`),
     FOREIGN KEY (`network_id`) REFERENCES `networks` (`id`)
 )
-    CHARACTER SET `utf8mb4` COLLATE `utf8mb4_general_ci`;
-
-CREATE TABLE `operation_systems`
-(
-    `id`   INTEGER AUTO_INCREMENT NOT NULL,
-    `name` NVARCHAR(200)          NOT NULL,
-    PRIMARY KEY (`id`)
-)
-    CHARACTER SET `utf8mb4` COLLATE `utf8mb4_general_ci`;
-
-CREATE TABLE `devices_operation_systems`
-(
-    `id`           INTEGER AUTO_INCREMENT NOT NULL,
-    `device_id`    INTEGER                NOT NULL,
-    `operation_id` INTEGER                NOT NULL,
-    `version`      NVARCHAR(200)          NOT NULL,
-    PRIMARY KEY (`id`),
-    FOREIGN KEY (`device_id`) REFERENCES `devices` (`id`),
-    FOREIGN KEY (`operation_id`) REFERENCES `operation_systems` (`id`)
-)
-    CHARACTER SET `utf8mb4` COLLATE `utf8mb4_general_ci`;
+    CHARACTER SET `utf8mb4`
+    COLLATE `utf8mb4_general_ci`;
